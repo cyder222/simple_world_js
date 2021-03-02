238176fb67f231ad9e481ae204f548a908558ad3
taiki mitsuyasu <boruboredo@gmail.com>
2021-03-02 22:41:08 +0900

    add mock wrapper

---
+++ CMakeFiles/World.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cyder/src/libs/simple_world_js/lib/World/src/cheaptrick.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/lib/World/src/cheaptrick.cpp.o"
+  "/home/cyder/src/libs/simple_world_js/lib/World/src/codec.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/lib/World/src/codec.cpp.o"
+  "/home/cyder/src/libs/simple_world_js/lib/World/src/common.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/lib/World/src/common.cpp.o"
+  "/home/cyder/src/libs/simple_world_js/lib/World/src/d4c.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/lib/World/src/d4c.cpp.o"
+  "/home/cyder/src/libs/simple_world_js/lib/World/src/dio.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/lib/World/src/dio.cpp.o"
+  "/home/cyder/src/libs/simple_world_js/lib/World/src/fft.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/lib/World/src/fft.cpp.o"
+  "/home/cyder/src/libs/simple_world_js/lib/World/src/harvest.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/lib/World/src/harvest.cpp.o"
+  "/home/cyder/src/libs/simple_world_js/lib/World/src/matlabfunctions.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/lib/World/src/matlabfunctions.cpp.o"
+  "/home/cyder/src/libs/simple_world_js/lib/World/src/stonemask.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/lib/World/src/stonemask.cpp.o"
+  "/home/cyder/src/libs/simple_world_js/lib/World/src/synthesis.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/lib/World/src/synthesis.cpp.o"
+  "/home/cyder/src/libs/simple_world_js/lib/World/src/synthesisrealtime.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/lib/World/src/synthesisrealtime.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "lib/World/src"
+  "src/hello"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/WorldJS.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cyder/src/libs/simple_world_js/src/WorldJS.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/WorldJS.dir/src/WorldJS.cpp.o"
+  "/home/cyder/src/libs/simple_world_js/src/hello/hello.cpp" "/home/cyder/src/libs/simple_world_js/CMakeFiles/WorldJS.dir/src/hello/hello.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "lib/World/src"
+  "src/hello"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/cyder/src/libs/simple_world_js/CMakeFiles/World.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
